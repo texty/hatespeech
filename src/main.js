7402ab86a2bc7dad32367460d66fe36a1489ad37
YevhDr <evgesha.drozdova@gmail.com>
2019-04-10 13:13:19 +0300

    init

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router";
+/* import router from "./router"; */
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = true;
 
-new Vue({
-  router,
+new Vue({ 
   render: h => h(App)
 }).$mount("#app");
